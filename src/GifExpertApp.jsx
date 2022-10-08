6f4860df536177312feb7f7a2f67e06971477215
Eros <eroslico1@gmail.com>
2022-10-08 10:21:36 -0300

    First Commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { AddCategory, GifGrid } from "./components";
+
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["God of War"]);
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
+    setCategories([newCategory, ...categories]);
+  };
+
+  return (
+    <>
+      {/* Titulo */}
+      <h1>GifExpertApp</h1>
+
+      {/* Input */}
+      <AddCategory onNewCategory={(event) => onAddCategory(event)} />
+
+      {/* Listado de Gif */}
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
+    </>
+  );
+};
